15481c1f11f04d40ef3f3c58512e2b633139ba39
Rukmani Devi <Rukmani@Commits-MacBook-Pro.local>
2023-01-18 23:03:22 -0500

    Added a Procfile.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const axios=require("axios");
 const app=express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 const baseUrl="https://www.googleapis.com/youtube/v3/";
 const api_key="AIzaSyDSlSpaOoZtLYPtTJ6TcxdJ2AZXAoFdTxU";
 app.get('/',(req,res)=>{
